953292adf144a4bdbb0ab4363499c4e9a6dd94c9
parkyosep <parkyosep362@gmail.com>
2023-06-22 19:58:59 +0900

    yosep06221958

--- src/com/ict5/client/Huge_main.java
+++ src/com/ict5/client/Huge_main.java
@@ -20,7 +20,7 @@ public class Huge_main extends JFrame {
 		Notice nt= new Notice(this);
 		Myp myp = new Myp(this);
 		WelCome wc = new WelCome(this);
-
+		Mypoint mp = new Mypoint(this);
 //		클래스명 변수명 = new 클래스명(this);  이 클래스들은 각각의 페이지(카드)를 의미합니다.
 //		클래스명 변수명 = new 클래스명(this);
 //		클래스명 변수명 = new 클래스명(this);
@@ -33,7 +33,7 @@ public class Huge_main extends JFrame {
 		pg1.add("ca",ca);
 		pg1.add("myp",myp);
 		pg1.add("wc",wc);
-
+		pg1.add("mp",mp);
 //		pg1.add("페이지명",객체이름);
 //		pg1.add("페이지명",객체이름);
 
@@ -43,7 +43,7 @@ public class Huge_main extends JFrame {
 		pg1.add("2",nt);
 		pg1.add("3",myp);
 		pg1.add("4",wc);
-		cardlayout.show(pg1, "4");
+		cardlayout.show(pg1, "mp");
 
 		add(pg1);
 		setVisible(true);
