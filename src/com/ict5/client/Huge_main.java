4f0102b8a6e1b634178beb92f26b7c74ebe66c0d
PLETTA0114 <pletta@naver.com>
2023-06-24 20:26:19 +0900

    로그인 화면 UI

--- src/com/ict5/client/Huge_main.java
+++ src/com/ict5/client/Huge_main.java
@@ -28,6 +28,7 @@ public class Huge_main extends JFrame {
 		Home home = new Home(this);
 		Chargepoint charp =new Chargepoint(this);
 		PassChange pc = new PassChange(this);
+		Login lo = new Login(this);
 		
 //		클래스명 변수명 = new 클래스명(this);
 //		클래스명 변수명 = new 클래스명(this);
@@ -44,9 +45,10 @@ public class Huge_main extends JFrame {
 		pg1.add("home",home); // 탭페이지
 		pg1.add("charp",charp); // 포인트충전
 		pg1.add("pc",pc); // 비밀번호 변경
+		pg1.add("lo",lo); // 로그인 페이지
 		
 		
-		cardlayout.show(pg1, "tab"); 
+		cardlayout.show(pg1, "lo"); 
 //		pg1.add("페이지명",객체이름);
 		
 
