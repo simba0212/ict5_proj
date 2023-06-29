95833547bae315af162f1c71262649cd4cdfabb4
Nyang7777 <ehdvy2@naver.com>
2023-06-29 19:30:31 +0900

    버튼 1차

--- src/com/ict5/admin/TimeTable.java
+++ src/com/ict5/admin/TimeTable.java
@@ -9,8 +9,8 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractCellEditor;
 import javax.swing.BorderFactory;
-import javax.swing.DefaultCellEditor;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -20,161 +20,152 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 
 public class TimeTable extends JPanel {
-	private JTable table;
-	Admin_main main;
-	CardLayout cardLayout;
+    private JTable table;
+    Admin_main main;
+    CardLayout cardLayout;
 
-	public TimeTable(Admin_main main) {
-		setBorder(BorderFactory.createLineBorder(Color.black));
+    public TimeTable(Admin_main main) {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        setBackground(Color.WHITE);
+        setLayout(new BorderLayout());
 
-		setBackground(Color.WHITE);
-		setLayout(new BorderLayout());
+        this.main = main;
+        this.cardLayout = main.cardlayout;
 
-		this.main = main;
-		this.cardLayout = main.cardlayout;
+        JPanel north = new JPanel(new BorderLayout());
+        north.setBackground(Color.WHITE);
+        JPanel north1 = new JPanel(new BorderLayout());
+        north1.setBackground(Color.WHITE);
+        JPanel north2 = new JPanel();
+        north2.setBackground(Color.WHITE);
+        north.setPreferredSize(new Dimension(10, 140));
 
-		JPanel north = new JPanel(new BorderLayout());
-		north.setBackground(Color.WHITE);
-		JPanel north1 = new JPanel(new BorderLayout());
-		north1.setBackground(Color.WHITE);
-		JPanel north2 = new JPanel();
-		north2.setBackground(Color.WHITE);
-		north.setPreferredSize(new Dimension(10, 140));
+        JLabel lblNewLabel = new JLabel("오늘의 강의목록");
+        lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(Font.BOLD, 17f));
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        north1.add(lblNewLabel, BorderLayout.WEST);
 
-		JLabel lblNewLabel = new JLabel("오늘의 강의목록");
-		lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(Font.BOLD, 17f));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		north1.add(lblNewLabel, BorderLayout.WEST);
+        JButton btnNewButton = new JButton("강의추가");
+        btnNewButton.setFont(btnNewButton.getFont().deriveFont(17f));
+        btnNewButton.setPreferredSize(new Dimension(100, 50));
 
-		JButton btnNewButton = new JButton("강의추가");
-		btnNewButton.setFont(btnNewButton.getFont().deriveFont(17f));
-		btnNewButton.setPreferredSize(new Dimension(100, 50));
+        north1.add(btnNewButton, BorderLayout.EAST);
 
-		
-		
-		north1.add(btnNewButton, BorderLayout.EAST);
+        JButton btnNewButton_1_1 = new JButton("<<");
+        btnNewButton_1_1.setPreferredSize(new Dimension(50, 30));
+        north2.add(btnNewButton_1_1);
 
-		JButton btnNewButton_1_1 = new JButton("<<");
-		btnNewButton_1_1.setPreferredSize(new Dimension(50, 30));
-		north2.add(btnNewButton_1_1);
+        JLabel lblNewLabel_2 = new JLabel();
+        lblNewLabel_2.setIcon(new ImageIcon("src/images/calender.png"));
+        north2.add(lblNewLabel_2);
 
-		JLabel lblNewLabel_2 = new JLabel(new ImageIcon("src/images/calender.png"));
-		north2.add(lblNewLabel_2);
+        JTextField date = new JTextField();
+        date.setColumns(20);
+        date.setPreferredSize(new Dimension(100, 50));
+        north2.add(date);
 
-		JTextField date = new JTextField();
-		date.setColumns(20);
-		date.setPreferredSize(new Dimension(100, 50));
-		north2.add(date);
+        JButton btnNewButton_1 = new JButton(">>");
+        btnNewButton_1.setPreferredSize(new Dimension(50, 30));
+        north2.add(btnNewButton_1);
+        north.add(north1, BorderLayout.NORTH);
+        north.add(north2, BorderLayout.SOUTH);
 
-		JButton btnNewButton_1 = new JButton(">>");
-		btnNewButton_1.setPreferredSize(new Dimension(50, 30));
-		north2.add(btnNewButton_1);
-		north.add(north1, BorderLayout.NORTH);
-		north.add(north2, BorderLayout.SOUTH);
+        add(north, BorderLayout.NORTH);
 
-		add(north, BorderLayout.NORTH);
+        Object[][] data = { { "09:00", "", "", "", "" }, { "10:00", "", "수업1", "", "" }, { "11:00", "", "", "수업2", "" },
+                { "12:00", "", "", "", "" }, { "13:00", "", "", "", "" }, { "14:00", "", "", "", "" },
+                { "15:00", "", "", "", "" }, { "16:00", "", "", "", "" }, { "17:00", "", "", "", "" },
+                { "18:00", "", "", "", "" }, { "19:00", "", "", "", "" }, { "20:00", "", "", "", "" },
+                { "21:00", "", "", "", "" } };
+        String[] columnNames = { "6월 10일", "P.T", "요가", "수영", "필라테스" };
 
-		Object[][] data = { { "09:00", "", "", "", "" }, { "10:00", "", "수업1", "", "" }, { "11:00", "", "", "수업2", "" },
-				{ "12:00", "", "", "", "" }, { "13:00", "", "", "", "" }, { "14:00", "", "", "", "" },
-				{ "15:00", "", "", "", "" }, { "16:00", "", "", "", "" }, { "17:00", "", "", "", "" },
-				{ "18:00", "", "", "", "" }, { "19:00", "", "", "", "" }, { "20:00", "", "", "", "" },
-				{ "21:00", "", "", "", "" } };
-		String[] columnNames = { "6월 10일", "P.T", "요가", "수영", "필라테스" };
+        DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                // 원하는 셀을 편집 가능하도록 설정
+                return column != 0;
+            }
+        };
 
-		DefaultTableModel model = new DefaultTableModel(data, columnNames) {
-			@Override
-			public boolean isCellEditable(int row, int column) {
-//				return !(data[row][column].toString().equals("") || column == 0);
-				return true;
-			}
-		};
+        TableCellRenderer buttonRenderer = new ButtonRenderer();
+        TableCellEditor buttonEditor = new ButtonEditor();
 
-		TableCellRenderer buttonRenderer = new ButtonRenderer();
-		TableCellEditor buttonEditor = new ButtonEditor(main);
+        table = new JTable(model);
+        table.setRowHeight(40);
+        table.setShowVerticalLines(false);
+        table.setDefaultRenderer(Object.class, buttonRenderer);
+        table.setDefaultEditor(Object.class, buttonEditor);
 
-		table = new JTable(model);
-		table.setRowHeight(40);
-		table.setShowVerticalLines(false);
-		table.setDefaultRenderer(Object.class, buttonRenderer);
-		table.setDefaultEditor(Object.class, buttonEditor);
+        // Add the table to a scroll pane and display it
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setPreferredSize(new Dimension(600, 544));
+        add(jsp, BorderLayout.SOUTH);
 
-		// Add the table to a scroll pane and display it
-		JScrollPane jsp = new JScrollPane(table);
-		jsp.setPreferredSize(new Dimension(600, 544));
-		add(jsp, BorderLayout.SOUTH);
-		
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(main.pg1, "classEdit");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(main.pg1, "classEdit");
+            }
+        });
+    }
 
-			}
-		});
-		
-	}
-	
-	
 }
 
-class ButtonRenderer extends JButton implements TableCellRenderer {
-
-	public ButtonRenderer() {
-		setOpaque(true);
-	}
-
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		// Render the specific cells in the third row as buttons
-		if (!(value.toString().equals("") || column == 0)) {
-			setText(value.toString());
-			return this;
-		} else if (column == 0) {
-			return new JLabel(value.toString(), JLabel.CENTER);
-		}
-
-		else {
-			// Render other cells normally
-			return new JLabel(value.toString());
-		}
-	}
+class ButtonRenderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+            int row, int column) {
+        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        
+        // 특정 열과 조건을 만족하는 경우에만 버튼 생성
+        if (column == 2 && value.toString().equals("수업1")|| column == 3 && value.toString().equals("수업2")) {
+            JButton button = new JButton(value.toString());
+            button.setOpaque(true);
+            return button;
+        } else {
+            return component;
+        }
+    }
 }
 
-class ButtonEditor extends DefaultCellEditor {
-	private JButton button;
+class ButtonEditor extends AbstractCellEditor implements TableCellEditor {
+    private JButton button;
 
-	public ButtonEditor(Admin_main main) {
-		super(new JTextField());
-		button = new JButton("Button");
-		button.addActionListener(e -> {
-			// Button action
-			JButton jb = (JButton) e.getSource();
+    public ButtonEditor() {
+        button = new JButton();
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 버튼 클릭 시 동작을 수행할 수 있도록 구현
+                System.out.println("Button clicked");
+                JOptionPane.showMessageDialog(button, "눌러");
+                fireEditingStopped();
+            }
+        });
+    }
 
-			main.cardlayout.show(main.pg1, "home");
-			JOptionPane.showMessageDialog(button, "눌러");
-			fireEditingStopped();
-		});
-	}
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        // Edit the specific cells in the desired columns using the button editor
+        if (column == 2 && value.toString().equals("수업1")|| column == 3 && value.toString().equals("수업2")) {
+            button.setText(value.toString());
+            return button;
+        } else {
+            // 해당 셀이 아닌 경우, 기본 편집 컴포넌트를 반환
+            return null;
+        }
+    }
 
-	@Override
-	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-		// Edit the specific cells in the third row using the button editor
-		if (!(value.toString().equals("") || column == 0)) {
-			button.setText(value.toString());
-			return button;
-		} else {
+    public Object getCellEditorValue() {
+        return button.getText();
+    }
 
-			return super.getTableCellEditorComponent(table, value, isSelected, row, column);
-		}
-	}
-
-	@Override
-	public Object getCellEditorValue() {
-		return button.getText();
-	}
-
+    public boolean stopCellEditing() {
+        // 셀 편집이 중지될 때 호출되는 메서드
+        // 필요한 경우 추가적인 작업을 수행할 수 있습니다.
+        return super.stopCellEditing();
+    }
 }
