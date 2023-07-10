5aa4971a82719cc08c7fde6c1133c4a3c4966467
Kim_Simba <ksimba0212@naver.com>
2023-07-10 21:50:16 +0900

    Update DAO.java

--- src/com/ict5/db/DAO.java
+++ src/com/ict5/db/DAO.java
@@ -79,42 +79,7 @@ public class DAO {
 	}
 
 	
-//	public static String near_type() {
-//		List<VO> list = null;
-//		list = getSession().selectList("mostclose");
-//		VO vo = list.get(0);
-//		String str = vo.getClass_type();
-//		return str;
-//	}
-//	public static String t_name_select() {
-//		List<VO> list = null;
-//		list = getSession().selectList("t_name_select");
-//		VO vo = list.get(0);
-//		String str = vo.getTeacher_name();
-//		return str;
-//	}
-//	public static String near_classtime() {
-//		List<VO> list = null;
-//		list = getSession().selectList("mostclose");
-//		VO vo = list.get(0);
-//		String str = vo.getClass_time();
-//		return str;
-//	}
-//	public static String near_classdate() {
-//		List<VO> list = null;
-//		list = getSession().selectList("mostclose");
-//		VO vo = list.get(0);
-//		String str = vo.getClass_date();
-//		return str;
-//	}
-//	public static String near_membername() {
-//		List<VO> list = null;
-//		list = getSession().selectList("mostclose");//변경필요
-//		VO vo = list.get(0);
-//		String str = vo.getMember_name();
-//		return str;
-//	}
-//	
+
 
 	public static List<VO> t_bookclass(VO vo) {
 		List<VO> list = null;
